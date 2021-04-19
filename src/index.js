aed849d45fcb15a94ca2a471ce3acf08a775df20
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2021-04-19 12:38:47 +0430

    base config for babel

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const kia = "this main part of code"
+
+
+
+class Kia {
+    kiamethod(){
+        console.log("kia is bala when live-server ")
+    }
+}
+
+const user = new Kia();
+user.kiamethod();
